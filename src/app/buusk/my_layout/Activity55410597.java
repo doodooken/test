0878f4d72e21bc267c3add2aa3414c09c1bd797a
doodooken <pasitaken@hotmail.com>
2014-08-27 15:55:03 +0700

    Test1

---
+++ src/app/buusk/my_layout/Activity55410597.java
@@ -0,0 +1,44 @@
+package app.buusk.my_layout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Activity55410597 extends Activity implements OnClickListener {
+	private Button btnone;
+	private Button btntwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        btnone	 = (Button) findViewById(R.id.btn_ken);
+        btntwo = (Button) findViewById(R.id.change1);
+        btntwo.setOnClickListener(this);
+        
+        
+		}	
+    		@Override
+			public void onClick(View v) {
+    			switch (v.getId()) {
+    			case R.id.change1:
+    				Intent i = new Intent(getApplicationContext(),Main.class);
+    				startActivity(i);
+    				break;
+    				
+    				default:
+    					break;
+    				
+				// TODO Auto-generated method stub
+				
+				
+				
+			}
+    }
+}
---
+++ src/app/buusk/my_layout/Main.java
@@ -0,0 +1,12 @@
+package app.buusk.my_layout;
+
+import android.os.Bundle;
+
+public class Main extends Activity55410597  {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.change);
+	}
+}
